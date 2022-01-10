9f60fa216c62ee9a8856f4effa59dbaef57c076b
Kubusiek <jakob.szatan@gmail.com>
2022-01-09 20:23:54 -0500

    V13.4.4 Dodano Skok opcje

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,14 @@ public class PlayerMovement : MonoBehaviour {
 
     public float forwardForce = 2000f;
     public float sidewaysForce = 500f;
+    public float upwardsForce = 30f;
+    public float mass;
 
+
+//use rb = GetComponent<RigidBody>();
+//and rb.mass = mass
+//then you can set the amount of mass using
+//rb.mass = 10f
 
     // Update is called once per frame
     void FixedUpdate ()
@@ -23,9 +30,51 @@ public class PlayerMovement : MonoBehaviour {
         rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
     }
 
+    if (Input.GetKey("space") ) {
+       jump();
+    }
+
+    void jump() {
+    if (rb.position.y <= 1.2f) {
+                
+        rb.AddForce(0, upwardsForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+        rb = GetComponent<Rigidbody>();
+        rb.mass = mass;
+        rb.mass = 1f;
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+    }
+
+
+void land() {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = mass;
+        rb.mass = 1f;
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        
+        }
+
+    
+
+    if (Input.GetKeyUp("space") )
+    {
+        landAir();
+    }
+
+    void landAir () {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = mass;
+        rb.mass = 400000f;
+        rb.AddForce(0, 0, 4000000 * Time.deltaTime);
+        land();
+    }
+
+   
+    
     if (rb.position.y < -1f) {
         FindObjectOfType<GameManager>().EndGame();
     }
 
     }
-}
+    }
+
